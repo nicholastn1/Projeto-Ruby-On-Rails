47139670becbedfdf7421c025b5a0200a2c5cb5d
Nicholas Timbo <nicholasnogueira12@gmail.com>
2021-07-26 11:02:55 -0300

    6d64f46e8efa2e561b0d03c4c3e40109

---
+++ app/assets/javascripts/users_backoffice.coffee
@@ -0,0 +1,9 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+//= require jquery-2.2.3/dist/jquery.min.js
+//= require bootstrap/dist/js/bootstrap.bundle.min.js
+//= require fastclick/lib/fastclick.js
+//= require nprogress/nprogress.js
+//= require malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.js
+//= require custom.min.js
---
+++ config/initializers/assets.rb
@@ -0,0 +1,25 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path.
+# Rails.application.config.assets.paths << Emoji.images_path
+# Add Yarn node_modules folder to the asset load path.
+Rails.application.config.assets.paths << Rails.root.join('node_modules')
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+
+# /app/assets
+Rails.application.config.assets.precompile += %w( admin.js admin.css 
+                                                  admins_backoffice.js admins_backoffice.css
+                                                  users_backoffice.js users_backoffice.css )
+
+# /lib/assets
+Rails.application.config.assets.precompile += %w( sb-admin-2.min.css sb-admin-2.min.js 
+                                                  custom.min.js custom.min.css img.jpg)
+
+# /vendor/assets
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery.min.js )
