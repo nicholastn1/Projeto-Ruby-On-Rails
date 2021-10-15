3636e0f84c3218c866db0b7bbedd7857b9269638
Nicholas Timbo <nicholasnogueira12@gmail.com>
2021-10-15 02:08:56 +0000

    Continuação do desenvolvimento da aplicação. Criação do filtro de perguntas por assunto, configurações na rota e ajustes de layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
+
   namespace :site do
     get  'welcome/index'
     get  'search', to: 'search#questions'
+    get  'subject/:subject_id/:subject', to: 'search#questions', as: 'search_subject'
     post 'answer', to: 'answer#question'
   end
   namespace :users_backoffice do
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
     resources :subjects  # Assuntos/Áreas
     resources :questions # Perguntas
   end
-  
+
   devise_for :admins
   devise_for :users
 
